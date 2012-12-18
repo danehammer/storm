72253ff7cad17ff7825890e93fe9824766d7ff15
Dane Hammer <dane.hammer@cerner.com>
2012-12-17 23:58:26 -0600

    first try at really doing this in thrift api, need to fix running it against 0.9.0

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,7 @@
 # kill old nimbus
 jps | grep [n]imbus | awk '{print $1}' | xargs kill
 
-lein1 jar
-cp storm-0.8*.jar /usr/local/Cellar/storm/0.8.1/libexec/
+lein jar
+cp target/storm-0.9*.jar /usr/local/Cellar/storm/0.8.1/libexec/
 
 storm nimbus &
